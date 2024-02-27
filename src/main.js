dcb6f4afb2de26b651df01e6991716f85cd3fe2f
Quentin Carver <qgcarver@protonmail.com>
2024-02-27 14:26:39 +0000

    Setup with wasmoon and swissgl

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
-import update from './update.js';
+import { LuaFactory } from "./wasmoon.js";
+import './swissgl.js'; const SwissGL = _SwissGL;
 
-// even though Rollup is bundling all your files together, errors and
-// logs will still point to your original source modules
-console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
+var touched = 0;
 
-update();
+window.onload = function () {
+    const factory = new LuaFactory();
+    const cnv = document.getElementById("screen");
+    if (!cnv) { showErr("couldn't get canvas"); return; }
+    cnv.ontouchstart = function () {
+        if (1<touched) {
+            window.location.reload();
+        }
+        touched = touched+1;
+        document.documentElement.requestFullscreen();
+    }
+    if (!factory) { showErr("can't create LuaFactory"); }
+    const glsl = SwissGL(cnv);
+    const data = new Uint8Array([255,0,0,0]);
+
+    const dat = glsl({},{size:[4,1],format:'r8',data, tag:"dat"});
+    glsl.loop(({time})=>{
+        glsl.adjustCanvas();
+        glsl({time, dat, Aspect:'cover',FP:`
+            vec2 pos = vec2(XY);
+            FOut = vec4(0);
+            FOut = vec4(sin(length(XY)*vec3(30,30.5,31)
+            -(time*6.)+atan(XY.x,XY.y)*3.),1);
+            
+            #define idx(i) dat(ivec2(i,0)).x
+            FOut = vec4(idx(0),idx(1),idx(2),1)`});
+    });
+}
