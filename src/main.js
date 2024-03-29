3534930ab515dba937ae6a9b188d8b371b269818
Quentin Carver <qgcarver@protonmail.com>
2024-03-29 13:32:10 -0700

    add js part of indent codemirror extension

--- src/main.js
+++ src/main.js
@@ -3,10 +3,49 @@ import './swissgl.js'; const SwissGL = _SwissGL;
 import {EditorView, basicSetup} from "codemirror";
 import {oneDark} from "@codemirror/theme-one-dark";
 import {keymap} from "@codemirror/view";
-import {indentWithTab} from "@codemirror/commands"
+import {indentWithTab} from "@codemirror/commands";
+import { foldService } from '@codemirror/language';
 import {vim, Vim, getCM, CodeMirror} from "@replit/codemirror-vim"
 import fnl from "./fennel.lua";
 import init from "./init.fnl";
+
+
+const foldingOnIndent = foldService.of((state, from, to) => {
+    const line = state.doc.lineAt(from) // First line
+    const lines = state.doc.lines // Number of lines in the document
+    const indent = line.text.search(/\S|$/) // Indent level of the first line
+    let foldStart = from // Start of the fold
+    let foldEnd = to // End of the fold
+
+    // Check the next line if it is on a deeper indent level
+    // If it is, check the next line and so on
+    // If it is not, go on with the foldEnd
+    let nextLine = line
+    while (nextLine.number < lines) {
+        nextLine = state.doc.line(nextLine.number + 1) // Next line
+        const nextIndent = nextLine.text.search(/\S|$/) // Indent level of the next line
+
+        // If the next line is on a deeper indent level, add it to the fold
+        if (nextIndent > indent) {
+            foldEnd = nextLine.to // Set the fold end to the end of the next line
+        } else {
+            break // If the next line is not on a deeper indent level, stop
+        }
+    }
+
+    // If the fold is only one line, don't fold it
+    if (state.doc.lineAt(foldStart).number === state.doc.lineAt(foldEnd).number) {
+        return null
+    }
+
+    // Set the fold start to the end of the first line
+    // With this, the fold will not include the first line
+    foldStart = line.to
+
+    // Return a fold that covers the entire indent level
+    return { from: foldStart, to: foldEnd }
+})
+
 
 
 var touched = 1;
@@ -153,7 +192,7 @@ window.onload = () => {
     ap(editor,cmwrap);
     const cm = new EditorView({
         extensions:
-          [vim(),oneDark,basicSetup,keymap.of([indentWithTab])],
+          [vim(),oneDark,basicSetup,keymap.of([indentWithTab]),foldingOnIndent],
         parent:cmwrap
     });
     let vimcm = getCM(cm);
